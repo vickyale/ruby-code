1ae16c6776a91229e642d413f17870d99a75ce59
Victoria Ubaldo <viky.ubaldo@gmail.com>
2017-12-13 00:56:56 -0500

    add operaciones y variables

---
+++ basics/operaciones_variables.rb
@@ -0,0 +1,68 @@
+25 + 13
+
+15 - 65
+
+25 * 5
+
+35 / 5
+
+25 / 9.0
+
+
+25 % 9
+
+5 ** 2
+
+Math.sqrt(9)
+
+10 == 8
+
+10!=8
+
+10>8
+
+10>=8
+
+true && false
+
+true and false
+
+true || false
+
+true or false
+
+false || false
+
+not true
+
+!true
+
+
+##Variables
+
+puts nombre
+nombre = "Vicky"
+
+
+#Conversiones
+
+##Si tiene un valor integer a string
+
+'Hola' + 2.to_s
+
+
+#Si tiene un valor string a integer
+'2'.to_i + 2
+
+
+#metodos puts
+ 
+ #comilla dobles solo cuando se neceste inyectar codigo ruby dentro de una cadena
+
+
+ nombre="Vicky"
+
+ puts "Hola #{nombre} "
+
+
+ puts "La suma es #{2+5}"

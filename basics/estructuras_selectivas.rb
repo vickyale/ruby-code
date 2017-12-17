97c22cdbf8681540c1a96607d5f8807354d5c5e0
Victoria Ubaldo <viky.ubaldo@gmail.com>
2017-12-17 00:28:31 -0500

    add estructuras selectivas

---
+++ basics/estructuras_selectivas.rb
@@ -0,0 +1,10 @@
+puts '¿Cual es tu nombre?'
+nombre = gets.chomp
+
+if nombre=='Vicky'
+    puts 'Bienvenida!'
+elsif nombre=='Alejandra'
+    puts 'Nombre incorrecto'
+else
+    puts 'Intente nuevamente'    
+end
